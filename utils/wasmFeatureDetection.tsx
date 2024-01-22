fc119440dec6ef30f9041d81587b29803166cb08
irfanfath <mirfanf7@gmail.com>
2024-01-22 18:48:40 +0700

    update wasm

--- utils/wasmFeatureDetection.tsx
+++ utils/wasmFeatureDetection.tsx
@@ -1,52 +1,66 @@
-/* eslint-disable react/react-in-jsx-scope */
-/* eslint-disable no-case-declarations */
+interface WasmFeatureDetect {
+    bigInt: () => Promise<boolean>;
+    bulkMemory: () => Promise<boolean>;
+    exceptions: () => Promise<boolean>;
+    multiValue: () => Promise<boolean>;
+    mutableGlobals: () => Promise<boolean>;
+    referenceTypes: () => Promise<boolean>;
+    saturatedFloatToInt: () => Promise<boolean>;
+    signExtensions: () => Promise<boolean>;
+    simd: () => Promise<boolean>;
+    tailCall: () => Promise<boolean>;
+    threads: () => Promise<boolean>;
+}
 
-type WasmFeatureDetect = {
-  bigInt: () => Promise<boolean>;
-  bulkMemory: () => Promise<boolean>;
-  exceptions: () => Promise<boolean>;
-  multiValue: () => Promise<boolean>;
-  mutableGlobals: () => Promise<boolean>;
-  referenceTypes: () => Promise<boolean>;
-  saturatedFloatToInt: () => Promise<boolean>;
-  signExtensions: () => Promise<boolean>;
-  simd: () => Promise<boolean>;
-  tailCall: () => Promise<boolean>;
-  threads: () => Promise<boolean>;
+const wasmFeatureDetect: WasmFeatureDetect = {
+    bigInt: async () => {
+        try {
+            const result = await WebAssembly.instantiate(new Uint8Array([0, 97, 115, 109, 1, 0, 0, 0, 1, 6, 1, 96, 1, 126, 1, 126, 3, 2, 1, 0, 7, 5, 1, 1, 98, 0, 0, 10, 6, 1, 4, 0, 32, 0, 11]));
+            const exports = result.instance.exports;
+            
+            if (exports && typeof exports.b === 'function') {
+                return exports.b(BigInt(0)) === BigInt(0);
+            } else {
+                return false;
+            }
+        } catch (e) {
+            return false;
+        }
+    },
+    bulkMemory: async () => {
+        return WebAssembly.validate(new Uint8Array([0, 97, 115, 109, 1, 0, 0, 0, 1, 4, 1, 96, 0, 0, 3, 2, 1, 0, 5, 3, 1, 0, 1, 10, 14, 1, 12, 0, 65, 0, 65, 0, 65, 0, 252, 10, 0, 0, 11]));
+    },
+    exceptions: async () => {
+        return WebAssembly.validate(new Uint8Array([0, 97, 115, 109, 1, 0, 0, 0, 1, 4, 1, 96, 0, 0, 3, 2, 1, 0, 10, 8, 1, 6, 0, 6, 64, 25, 11]));
+    },
+    multiValue: async () => {
+        return WebAssembly.validate(new Uint8Array([0, 97, 115, 109, 1, 0, 0, 0, 1, 6, 1, 96, 0, 2, 127, 127, 3, 2, 1, 0, 10, 8, 1, 6, 0, 65, 0, 65, 0, 11]));
+    },
+    mutableGlobals: async () => {
+        return WebAssembly.validate(new Uint8Array([0, 97, 115, 109, 1, 0, 0, 0, 2, 8, 1, 1, 97, 1, 98, 3, 127, 1, 6, 6, 1, 127, 1, 65, 0, 11, 7, 5, 1, 1, 97, 3, 1]));
+    },
+    referenceTypes: async () => {
+        return WebAssembly.validate(new Uint8Array([0, 97, 115, 109, 1, 0, 0, 0, 1, 4, 1, 96, 0, 0, 3, 2, 1, 0, 10, 7, 1, 5, 0, 208, 112, 26, 11]));
+    },
+    saturatedFloatToInt: async () => {
+        return WebAssembly.validate(new Uint8Array([0, 97, 115, 109, 1, 0, 0, 0, 1, 4, 1, 96, 0, 0, 3, 2, 1, 0, 10, 12, 1, 10, 0, 67, 0, 0, 0, 0, 252, 0, 26, 11]));
+    },
+    signExtensions: async () => {
+        return WebAssembly.validate(new Uint8Array([0, 97, 115, 109, 1, 0, 0, 0, 1, 4, 1, 96, 0, 0, 3, 2, 1, 0, 10, 8, 1, 6, 0, 65, 0, 192, 26, 11]));
+    },
+    simd: async () => {
+        return WebAssembly.validate(new Uint8Array([0, 97, 115, 109, 1, 0, 0, 0, 1, 4, 1, 96, 0, 0, 3, 2, 1, 0, 10, 9, 1, 7, 0, 65, 0, 253, 15, 26, 11]));
+    },
+    tailCall: async () => {
+        return WebAssembly.validate(new Uint8Array([0, 97, 115, 109, 1, 0, 0, 0, 1, 4, 1, 96, 0, 0, 3, 2, 1, 0, 10, 6, 1, 4, 0, 18, 0, 11]));
+    },
+    threads: async () => {
+        try {
+            return "undefined" != typeof MessageChannel && new MessageChannel().port1.postMessage(new SharedArrayBuffer(1)), WebAssembly.validate(new Uint8Array([0, 97, 115, 109, 1, 0, 0, 0, 1, 4, 1, 96, 0, 0, 3, 2, 1, 0, 5, 4, 1, 3, 1, 1, 10, 11, 1, 9, 0, 65, 0, 254, 16, 2, 0, 26, 11]));
+        } catch (e) {
+            return false;
+        }
+    },
 };
 
-const WasmFeatureDetectComponent: WasmFeatureDetect = {
-  bigInt: async () => {
-    try {
-      return (
-        (await WebAssembly.instantiate(
-          new Uint8Array([0, 97, 115, 109, 1, 0, 0, 0, 1, 6, 1, 96, 1, 126, 1, 126, 3, 2, 1, 0, 7, 5, 1, 1, 98, 0, 0, 10, 6, 1, 4, 0, 32, 0, 11])
-        )).instance.exports.b(BigInt(0)) === BigInt(0)
-      );
-    } catch (e) {
-      return false;
-    }
-  },
-  bulkMemory: async () => WebAssembly.validate(new Uint8Array([/* ... your array here */])),
-  exceptions: async () => WebAssembly.validate(new Uint8Array([/* ... your array here */])),
-  multiValue: async () => WebAssembly.validate(new Uint8Array([/* ... your array here */])),
-  mutableGlobals: async () => WebAssembly.validate(new Uint8Array([/* ... your array here */])),
-  referenceTypes: async () => WebAssembly.validate(new Uint8Array([/* ... your array here */])),
-  saturatedFloatToInt: async () => WebAssembly.validate(new Uint8Array([/* ... your array here */])),
-  signExtensions: async () => WebAssembly.validate(new Uint8Array([/* ... your array here */])),
-  simd: async () => WebAssembly.validate(new Uint8Array([/* ... your array here */])),
-  tailCall: async () => WebAssembly.validate(new Uint8Array([/* ... your array here */])),
-  threads: async () => {
-    try {
-      return (
-        "undefined" != typeof MessageChannel &&
-        new MessageChannel().port1.postMessage(new SharedArrayBuffer(1)),
-        WebAssembly.validate(new Uint8Array([/* ... your array here */]))
-      );
-    } catch (e) {
-      return false;
-    }
-  },
-};
-
-export default WasmFeatureDetectComponent;
+export default wasmFeatureDetect;
